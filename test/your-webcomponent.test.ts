a3322800c275f822776c51cfe9d3af4ac0847fd7
banazsinjary <banazsinjary@gmail.com>
2021-06-16 14:33:33 -0700

    plant tracker table
    established a plant tracker table

--- test/your-webcomponent.test.ts
+++ test/your-webcomponent.test.ts
@@ -1,7 +1,7 @@
-import { html, fixture, expect } from '@open-wc/testing';
+/* import { html, fixture, expect } from '@open-wc/testing';
 
-import type { YourWebComponent } from '../src/your-webcomponent';
-import '../src/your-webcomponent';
+import type { YourWebComponent } from '../src/plant-tracker';
+import '../src/plant-tracker';
 
 describe('YourWebComponent', () => {
   it('has a default title "Hey there" and counter 5', async () => {
@@ -37,4 +37,4 @@ describe('YourWebComponent', () => {
 
     await expect(el).shadowDom.to.be.accessible();
   });
-});
+}); */
